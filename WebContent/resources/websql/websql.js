3b66d7a7166997f11bafde8e47b167893f12a4ce
S3Ekin <sekin4866111@163.com>
2017-08-09 14:18:28 +0800

    搭建websal

--- WebContent/resources/websql/websql.js
+++ WebContent/resources/websql/websql.js
@@ -3,57 +3,98 @@
  */
 (function () {
     //打开数据库
-    var sd=openDatabase("medical",1.1,"medical",10*1024*1024,function (result) {
+    var openDB=openDatabase("medical",1.1,"medical",10*1024*1024,function (result) {
         console.log(result)
     });
-    function createtable () {
-        alert(2);
     //建表
-    var createTableSQL= "CREATE TABLE table('id','title','zb','data')";
-    sd.transaction(function (taransactionobj) {
-        taransactionobj.executeSql(createTableSQL);
-    })
-    }
-    function add(obj) {
-        var addSQL="INSERT INTO recoder('id','title','des') VALUES (2,'会议记录2','大家今天需要开会')";
-        sd.transaction(function (ts) {
-            ts.executeSql(addSQL);
+   var webSql={
+       text:function () {
+           alert("success");
+       },
+       createTable:function(name){
+        var createTableSQL= "CREATE TABLE "+name+"('text','type','zb','data','x_wd','y_wd')";
+        openDB.transaction(function (taransactionobj) {
+            taransactionobj.executeSql(createTableSQL);
         })
-    }
-    function deleteData() {
-        var deleteSQL="DELETE FROM recoder WHERE id=2";
-        sd.transaction(function (ts) {
-            ts.executeSql(deleteSQL);
-        })
-    }
-    function searchData() {
-        /*
-         * sqlStatement:sql语句
-         * arguments:实参
-         * callback:回调函数
-         * errorCallback:错误时候的回调函数
-         * */
-        var searchSQL = "SELECT * FROM recoder";
-        sd.transaction(function (ts) {
-            ts.executeSql(searchSQL,[],function (ts,result) {
-                //result得到的结果集
-                //可以通过rows 得到结果数组
-                console.log(result.rows);
-                 var list =document.querySelector(".result-content");
-                var allHtml="";
-                for (var i = 0; i < result.rows.length; i++) {
-                    var info=result.rows[i];
-                    var item="<li><h1>"+info.title+"</h1><p>"+info.des+"</p></li>";
-                    allHtml+=item;
-                }
-                list.innerHTML=allHtml;
-            });
-        })
-    }
-    function updataData() {
+    },
+       add:function(obj,tableName) {
+           var addSQL="INSERT INTO "+tableName+"('text','type','zb','data','x_wd','y_wd') VALUES ('"+obj.text+"','"+obj.type+"','"+obj.zb+"','"+obj.data+"','"+obj.x_wd+"','"+obj.y_wd+"')";
+           console.log(addSQL);
+           openDB.transaction(function (ts) {
+               ts.executeSql(addSQL);
+           })
+       },
+       deleteData:function () {
+           var deleteSQL="DELETE FROM recoder WHERE id=2";
+           openDB.transaction(function (ts) {
+               ts.executeSql(deleteSQL);
+           })
+       },
+       searchData:function (tableName) {
+           /*
+            * sqlStatement:sql语句
+            * arguments:实参
+            * callback:回调函数
+            * errorCallback:错误时候的回调函数
+            * */
+           var searchSQL = "SELECT * FROM "+tableName;
+           openDB.transaction(function (ts) {
+               ts.executeSql(searchSQL,[],function (ts,result) {
+                   //result得到的结果集
+                   //可以通过rows 得到结果数组
+                   console.log(result.rows);
+
+               });
+           })
+       },
+       searchTitle:function (tableName) {
+           var searchSQL = "SELECT text FROM "+tableName;
+           return new Promise(function (res, reject) {
+               if (openDB) {
+                   openDB.transaction(function (ts) {
+                       ts.executeSql(searchSQL,[],function (ts,result) {
+                           res({code: 2000, message: "查询成功", data: result.rows});
+                       }, function (error) {
+                           reject({code: 2001, message: "查询失败", data: error})
+                       });
+                   });
+               } else {
+                   reject({code: 3000, message: "打开数据库失败！"})
+               }
+           });
+          /* openDB.transaction(function (ts) {
+               ts.executeSql(searchSQL,[],function (ts,result) {
+                   console.log(result.rows);
+               });
+           });*/
+       },
+       searchSinge:function (tableName,title) {
+           var searchSQL = "SELECT * FROM "+tableName+" WHERE text='"+title+"'";
+           return new Promise(function (res, reject) {
+               if (openDB) {
+                   openDB.transaction(function (ts) {
+                       ts.executeSql(searchSQL,[],function (ts,result) {
+                           res({code: 2000, message: "查询成功", data: result.rows});
+                       }, function (error) {
+                           reject({code: 2001, message: "查询失败", data: error})
+                       });
+                   });
+               } else {
+                   reject({code: 3000, message: "打开数据库失败！"})
+               }
+           });
+          /* openDB.transaction(function (ts) {
+               ts.executeSql(searchSQL,[],function (ts,result) {
+                   console.log(result.rows);
+               });
+           });*/
+       },
+       updataData:function () {
         var updata="UPDATE recoder SET title='浪费时间' WHERE id=2";
-        sd.transaction(function (ts) {
+        openDB.transaction(function (ts) {
             ts.executeSql(updata);
         })
     }
+   };
+    window.webSql=webSql;
 })();
